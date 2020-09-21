62855c3889f8a84d1476ada76e683014d8be720c
Mike <tankf33der@disroot.org>
2020-09-21 22:05:26 +0300

    .

--- monocypher.go
+++ monocypher.go
@@ -1,1 +1,27 @@
+package main
+
+import (
+	// #cgo pkg-config: monocypher
+	// #include <monocypher.h>
+	"C"
+	//"unsafe"
+	"fmt"
+)
+
+func sha512(in []byte) []byte {
+	out := make([]byte, 64)
+	C.crypto_sha512(
+		(*C.uchar)(&out[0]),
+		(*C.uchar)(&in[0]),
+		(C.ulonglong)(len(in)))
+	return out
+}
+
+func main() {
+	o := sha512(nil)
+	fmt.Println("start")
+	fmt.Println(o)
+}
+
+
 
