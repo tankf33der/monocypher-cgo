290794422a88035f34513ecafd65420c7a69355f
Mike <tankf33der@disroot.org>
2020-09-21 22:48:05 +0300

    .

--- monocypher.go
+++ monocypher.go
@@ -4,13 +4,23 @@ package main
 // #include <monocypher.h>
 // #include <monocypher-ed25519.h>
 import "C"
+import "unsafe"
 import "fmt"
 
 func sha512(in []byte) []byte {
+	var ptr unsafe.Pointer
 	out := make([]byte, 64)
+
+	if in == nil {
+		ptr = unsafe.Pointer(&in)
+	} else {
+		ptr = unsafe.Pointer(&in[0])
+	}
+
 	C.crypto_sha512(
 		(*C.uchar)(&out[0]),
-		(*C.uchar)(&in[0]),
+		// (*C.uchar)(unsafe.Pointer(&in[0])),
+		(*C.uchar)(ptr),
 		(C.ulong)(len(in)))
 	return out
 }
